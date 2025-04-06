a3ff101d500f111efeb72da38be67a8310a96100
kasia <kasiazuc@gmail.com>
2025-04-06 08:05:03 +0200

    add cellview to diff file

---
+++ include/cellview.h
@@ -0,0 +1,25 @@
+#ifndef CELLVIEW_H
+#define CELLVIEW_H
+
+#include <QWidget>
+
+class CellView : public QWidget {
+public:
+    CellView(QWidget* parent = nullptr);
+    
+    void setWall(bool value);
+    void setGoal(bool value);
+    void setMouse(bool value);
+    void setVisitCount(int count);
+    
+protected:
+    void paintEvent(QPaintEvent* event) override;
+    
+private:
+    bool isWall;
+    bool isGoal;
+    bool isMouse;
+    int visitCount;
+};
+
+#endif // CELLVIEW_H
---
+++ src/cellview.cpp
@@ -0,0 +1,57 @@
+#include "cellview.h"
+#include <QPainter>
+#include <QColor>
+
+CellView::CellView(QWidget* parent) 
+    : QWidget(parent), isWall(false), isGoal(false), isMouse(false), visitCount(0) {
+    setMinimumSize(20, 20);
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+}
+
+void CellView::setWall(bool value) { 
+    isWall = value; 
+    update(); 
+}
+
+void CellView::setGoal(bool value) { 
+    isGoal = value; 
+    update(); 
+}
+
+void CellView::setMouse(bool value) { 
+    isMouse = value; 
+    update(); 
+}
+
+void CellView::setVisitCount(int count) { 
+    visitCount = count; 
+    update(); 
+}
+
+void CellView::paintEvent(QPaintEvent* event) {
+    QPainter painter(this);
+    
+    if (isWall) {
+        painter.fillRect(rect(), Qt::black);
+    } else if (isGoal) {
+        painter.fillRect(rect(), Qt::green);
+    } else if (isMouse) {
+        painter.fillRect(rect(), Qt::red);
+    } else if (visitCount > 0) {
+        // Create a gradient of blue based on visit count
+        // More visits = darker blue
+        int intensity = std::max(255 - (visitCount * 30), 100); // Don't go too dark
+        painter.fillRect(rect(), QColor(intensity, intensity, 255));
+    } else {
+        painter.fillRect(rect(), Qt::white);
+    }
+    
+    painter.setPen(Qt::gray);
+    painter.drawRect(rect().adjusted(0, 0, -1, -1));
+    
+    // Optionally show visit count as text for cells visited more than once
+    if (visitCount > 1) {
+        painter.setPen(Qt::black);
+        painter.drawText(rect(), Qt::AlignCenter, QString::number(visitCount));
+    }
+}
